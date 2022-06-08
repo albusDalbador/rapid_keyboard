792eb7d0fc6c489d1dab763d6cee79863e48a0d2
albusDalbador <lemiasheuski@student.agh.edu.pl>
2022-06-08 21:21:09 +0200

    update text field

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-document.addEventListener('DomContentLoaded', () => {
-    
+window.addEventListener('DOMContentLoaded', () => {
+
+    //page elements
+    var textWrapper = document.querySelector('.text_field')
+
+    var textArray = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis culpa in eveniet cum quam, deleniti dignissimos eligendi voluptatibus veniam maiores consequuntur voluptate assumenda dolore itaque minima autem quae perspiciatis doloremque?'.split('')
+
+    currentLetter = textArray[0]
+
+    var controlArray = []
+
+
+    //
+    textArray.forEach(item => addNewDivElemWithClass(textWrapper,'letter_field',item))
+
+
+
+
+    document.addEventListener('keypress', (event) => {
+        console.log(event.code.slice(3))
+        if (event.code.slice(3).toLowerCase() == currentLetter) {
+            //update color
+        } else {
+            //error code
+        }
+    })
+
+    // const 
+
+
 })
+
+
+function addNewDivElemWithClass(parent,className,textContent) {
+    var div = document.createElement('div')
+    div.classList.add(className)
+    div.textContent = textContent
+    parent.append(div)
+}
