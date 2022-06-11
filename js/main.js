3f773b5caf741a6919cb540e5456dc0180804fc3
albusDalbador <lemiasheuski@student.agh.edu.pl>
2022-06-11 20:37:56 +0200

    keyboard logic improved

--- js/main.js
+++ js/main.js
@@ -1,58 +1,92 @@
+
+function isUpper(str) {
+    return !/[a-z]/.test(str) && /[A-Z]/.test(str);
+}
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
-    //page elements
+    //page elements selectors
     var textWrapper = document.querySelector('.text_field')
 
-    var textArray = 'Lorem ipsum dolor'.split('')
 
-    //
-    textArray.forEach(item => addNewDivElemWithClass(textWrapper,'letter_field',item))
-    letterFields = document.querySelectorAll('.letter_field')
+    //other variables
+    var textArray = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi pariatur itaque, officiis et aut quo fugiat asperiores quia eum reprehenderit blanditiis eos voluptates, illo natus necessitatibus sequi possimus eius adipisci?'.split('')
+    letterFields = []
 
-
+    textArray.forEach(item => {
+        var div = document.createElement('div')
+        div.classList.add('letter_field')
+        div.textContent = item
+        letterFields.push(div)
+        textWrapper.append(div)
+    })
+    letterFields[0].classList.add('text_field_current')
     currentLetterIndex = 0
-    // console.log(currentLetter)
-    document.addEventListener('keypress', (event) => {
-        console.log(event.keyCode)
-        if (event.keyCode == letterFields[currentLetterIndex].textContent.toLowerCase().charCodeAt(0)) {
+    //for simplier notation
+    //
+    currentLetter = letterFields[0].textContent
+    //
+    errorCounter = 0
+
+
+    //keyboard listener
+    document.addEventListener('keypress',(event) => { 
+        
+        if (event.key == currentLetter) {
             //update color
             letterFields[currentLetterIndex].classList.remove('text_field_error')
+            letterFields[currentLetterIndex].classList.remove('text_field_current')
             letterFields[currentLetterIndex].classList.add('text_field_completed')
-            ++currentLetterIndex
+            letterFields[++currentLetterIndex].classList.add('text_field_current')
+            currentLetter = letterFields[currentLetterIndex].textContent
+            // console.log(currentLetter)
         } else {
             //error code
+            ++errorCounter
             letterFields[currentLetterIndex].classList.add('text_field_error')
         }
 
-        // if (currentLetterIndex >= textArray.length) {
-        //     document.removeEventListener('keypress')
-        // }
+        if (currentLetterIndex >= textArray.length) { //error here
+
+        }
     })
 
 })
+
+
+
+
+
 
 // function keyboardListenerLogic(event) {
 //     console.log(event.keyCode)
 //         if (event.keyCode == letterFields[currentLetterIndex].textContent.toLowerCase().charCodeAt(0)) {
 //             //update color
-//             letterFields[currentLetterIndex].classList.remove('text_field_error')
-//             letterFields[currentLetterIndex].classList.add('text_field_completed')
-//             ++currentLetterIndex
+//             letterFields[index].classList.remove('text_field_error')
+//     letterFields[index].classList.add('text_field_completed')
+//     ++index
 //         } else {
 //             //error code
 //             letterFields[currentLetterIndex].classList.add('text_field_error')
 //         }
 
-//         if (currentLetterIndex >= textArray.length) {
-//             document.removeEventListener('keypress')
+//         if (currentLetterIndex >= textArray.length) { //error here
+
+//             document.removeEventListener('keypress',keyboardListenerLogic)
 //         }
 // }
 
+// function updateCompletedLetterField(letters,index) {
+    
+// }
 
-function addNewDivElemWithClass(parent,className,textContent) {
-    var div = document.createElement('div')
-    div.classList.add(className)
-    div.textContent = textContent
-    // arrayOfElements.append(div)
-    parent.append(div)
-}
+
+// function addNewDivElemWithClass(parent,className,textContent) {
+//     var div = document.createElement('div')
+//     div.classList.add(className)
+//     div.textContent = textContent
+//     // arrayOfElements.append(div)
+//     parent.append(div)
+// }
